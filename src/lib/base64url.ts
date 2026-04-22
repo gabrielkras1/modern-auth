e91f4e4173711075fadf9f03634287146654279f
gabrielkras1 <gabriel.kras@hotmail.com>
2026-04-22 16:51:10 -0300

    reset

--- src/lib/base64url.ts
+++
@@ -1,7 +0,0 @@
-export function bufferToBase64URL(buffer: ArrayBuffer) {
-  return Buffer.from(buffer).toString("base64url");
-}
-
-export function base64URLToBuffer(base64url: string) {
-  return Buffer.from(base64url, "base64url");
-}
--- src/lib/webauthn.ts
+++
@@ -1,3 +0,0 @@
-export const rpID = process.env.WEBAUTHN_RP_ID!;
-export const origin = process.env.WEBAUTHN_ORIGIN!;
-export const rpName = "Modern Auth";
